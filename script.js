6d3f3440c8c1a207234e5112bf9bc741a1b55887
venkatagiribabu-bandaru-au46 <bandaruvenkatagiribabu999@gmail.com>
2022-06-04 07:24:43 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("true OR true =>",true||true);
+console.log("true OR false =>",true||false);
+console.log("false OR true =>",false||true);
+console.log("false OR false =>",false||false);
+
